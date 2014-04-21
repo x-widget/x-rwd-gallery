88b9be99d7a0136e425f637163281af03edfae25
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-04-21 16:42:46 +0800

    variable fixes

--- x-rwd-gallery.php
+++ x-rwd-gallery.php
@@ -4,8 +4,8 @@ include_once(G5_LIB_PATH.'/thumbnail.lib.php');
 
 widget_css();
 
-if( $widget_config['forum1'] ) $bo_table = $widget_config['forum1'];
-else $bo_table = bo_table(1);
+if( $widget_config['forum1'] ) $_bo_table = $widget_config['forum1'];
+else $_bo_table = bo_table(1);
 
 if( $widget_config['width'] ) $post_width = $widget_config['width'];
 else $post_width = 240;
@@ -16,7 +16,7 @@ else $post_height = 180;
 $limit = 4;
 
 $list = g::posts( array(
-			"bo_table" 	=>	$bo_table,
+			"bo_table" 	=>	$_bo_table,
 			"limit"		=>	$limit,
 			"select"	=>	"idx,domain,bo_table,wr_id,wr_parent,wr_is_comment,wr_comment,ca_name,wr_datetime,wr_hit,wr_good,wr_nogood,wr_name,mb_id,wr_subject,wr_content"
 				)
@@ -30,14 +30,14 @@ $list = g::posts( array(
 	for ($i=0; $i<count($list); $i++) {
 		
 		if ( $count_image >= 4 ) break;
-			$thumb = get_list_thumbnail($bo_table, $list[$i]['wr_id'], $post_width, $post_height);    					            
+			$thumb = get_list_thumbnail($_bo_table, $list[$i]['wr_id'], $post_width, $post_height);    					            
 			if($thumb['src']) {
 				$img = '<img class="img_left" src="'.$thumb['src'].'">';
 				$count_image ++;
 			}
 			else {
 				$img = '<img class="img_left" src="'.x::url()."/widget/".$widget_config['name'].'/img/no-image.png"/>';
-				$no_image = g::thumbnail_from_image_tag( $img, $bo_table, $post_width-2, $post_height-1 );
+				$no_image = g::thumbnail_from_image_tag( $img, $_bo_table, $post_width-2, $post_height-1 );
 				$img = "<img class='img_left' src='$no_image'/>";
 				$count_image ++;
 			}
